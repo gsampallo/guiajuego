42481ee21b7a02f020c07f27f589927b8dd082c9
Guillermo Sampallo <guille@gmail.com>
2018-06-29 21:49:33 -0300

    Signed-off-by: Guillermo Sampallo <guille@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # guiajuego
+
+La idea es programar un juego para Arduino, paso a paso donde se puede ir viendo el progreso y el uso de los distintos componentes de hardware y algoritmos en el software.
+
+29-06-2018
+guia1_demoDisplayJoystick 
+Muestra como conectar la pantalla oled por medio de i2c al arduino nano (tambien se puede aplicar a Arduino UNO), como dibujar una pequeña esfera en la pantalla y controlarla por medio del joystick.
+
+![alt text](https://raw.githubusercontent.com/gsampallo/guiajuego/master/protoboard.png "Esquematico")
+
+[![Video](https://img.youtube.com/vi/HjorjRJRUFs/0.jpg)](https://www.youtube.com/watch?v=HjorjRJRUFs)
---
+++ guia1_demoDisplayJoystick/guia1_demoDisplayJoystick.ino
@@ -0,0 +1,41 @@
+/*
+ * Demo de la pantalla oled i2c con el joystick. Mueve una esfera segun la posicion que indica el joystick.
+ * Arduino A4 a pantalla Oled SDA
+ * Arduino A5 a pantalla Oled SCK
+ * Arduino pin A0 a VRx
+ * Arduino pin A1 a VRy
+ * Arduino D2 a SW
+ * 
+ * Guillermo Sampallo
+ */
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();  
+}
+
+int i = 1;
+
+void loop() {
+
+  display.clearDisplay();
+
+  int posicionX = analogRead(A0);
+  int posicionY = analogRead(A1);
+
+  int x = map(posicionX, 0, 1024, 0, 128);
+  int y = map(posicionY, 0, 1024, 0, 32);
+
+  display.drawCircle(x, y, 6, WHITE);
+
+  display.display();
+  delay(10);
+}
